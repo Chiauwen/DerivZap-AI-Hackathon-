d3e8ad7ea0e4443728b171cb872844e601044202
weiizhengggg <kikuchitouma00@gmail.com>
2025-02-09 01:49:56 +0800

    Update tailwind.config.js

--- derivzap-app/tailwind.config.js
+++ derivzap-app/tailwind.config.js
@@ -1,19 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: [],
-=======
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
->>>>>>> a40c7c3fa41836be6c35ff00291102e36c9fe161
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> a40c7c3fa41836be6c35ff00291102e36c9fe161
