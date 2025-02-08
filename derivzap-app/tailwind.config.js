35a0cda4eb259adc959c614a867eaf307d380f8e
weiizhengggg <kikuchitouma00@gmail.com>
2025-02-09 02:03:31 +0800

    Update font

--- derivzap-app/tailwind.config.js
+++ derivzap-app/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"IBM Plex Sans"', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
